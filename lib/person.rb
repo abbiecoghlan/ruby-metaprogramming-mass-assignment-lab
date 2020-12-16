7752417c0d4f26817f1819fd1b9136887081d03d
“Abbie <abbie.coghlan@gmail.com>
2020-12-16 15:40:41 -0600

    commit tree 98b7d3eeef7a14d659a646ef86dd252522dc721a

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-  #your code here
-end
+  
+  @@all = []
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)      
+    end
+    @@all << self 
+  end
+end 
